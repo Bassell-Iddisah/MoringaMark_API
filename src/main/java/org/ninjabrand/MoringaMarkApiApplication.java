05e42df4d7829e1751a9ba5f33e44b7b32bb7b45
Bassell-Iddisah <bbasssell16@gmail.com>
2025-07-27 23:35:15 +0000

    Initial Commit

---
+++ src/main/java/org/ninjabrand/MoringaMarkApiApplication.java
@@ -0,0 +1,13 @@
+package org.ninjabrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoringaMarkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoringaMarkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ninjabrand/MoringaMarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ninjabrand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoringaMarkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
