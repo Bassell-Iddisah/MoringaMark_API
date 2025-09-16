088af66dfc46d660cf61afc7ed3b0b3a93e99db6
Bassell-Iddisah <bbasssell16@gmail.com>
2025-09-16 12:36:22 +0000

    random commit

--- src/main/java/org/ninjabrand/MoringaMarkApiApplication.java
+++ src/main/java/org/ninjabrand/MoringaMarkApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoringaMarkApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoringaMarkApiApplication.class, args);
     }
